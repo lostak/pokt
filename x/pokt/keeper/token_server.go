2a93b5ff760c847ef131b98b357d7524646cb3d5
lostak <lostak@engineer.com>
2023-01-20 16:55:02 -0800

    add amount_service

--- x/pokt/keeper/token_server.go
+++ x/pokt/keeper/token_server.go
@@ -31,30 +31,6 @@ func (k *Keeper) CreateToken(ctx context.Context, msg *MsgCreateToken) (*MsgCrea
 	return &MsgCreateTokenResponse{Portfolio: portfolio}, nil
 }
 
-func (k *Keeper) CreateAmount(ctx context.Context, msg *MsgCreateAmount) (*MsgCreateAmountResponse, error) {
-	fmt.Printf("Received: %v\n", msg.String())
-
-	portfolio, err := store.GetPortfolio()
-	if err != nil {
-		fmt.Println(err.Error())
-		return &MsgCreateAmountResponse{}, err
-	}
-
-	err = portfolio.AddTokenAmount(msg.GetAccount(), msg.GetChain(), msg.GetToken(), msg.GetAmount())
-	if err != nil {
-		fmt.Println(err.Error())
-		return &MsgCreateAmountResponse{}, err
-	}
-
-	if err := store.SetPortfolio(portfolio); err != nil {
-		fmt.Println(err.Error())
-		return &MsgCreateAmountResponse{}, err
-	}
-
-	portfolio.Println()
-	return &MsgCreateAmountResponse{Portfolio: portfolio}, nil
-}
-
 func (k *Keeper) UpdateCoinGeckoId(ctx context.Context, msg *MsgUpdateCoinGeckoId) (*MsgUpdateCoinGeckoIdResponse, error) {
 	fmt.Printf("Received: %v\n", msg.String())
 
