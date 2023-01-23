feaca1dee0f1a4096d719ab4e961457411d85a2f
lostak <lostak@engineer.com>
2023-01-23 01:22:58 -0800

    fix bash paths

--- run/buildPortfolio.sh
+++ run/buildPortfolio.sh
@@ -1,8 +1,10 @@
+# TO BE CALLED FROM BASE DIR
 echo "build a portfolio w/ bash commands"
 echo "add 4 accounts"
-bash ./addAccounts.sh
+bash .run/addAccounts.sh
 echo "add 4 chains to each account"
-bash ./addChains.sh
+bash .run/addChains.sh
 echo "add 1 token to each chain on each account"
-bash ./addTokens.sh
+bash .run/addTokens.sh
 echo "add amounts to each token on each chain on each account"
+bash .run/addAmounts.sh
