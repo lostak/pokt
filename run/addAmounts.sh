c32a26379dddce691293942f22d900e758904ea5
lostak <lostak@engineer.com>
2023-01-23 02:12:25 -0800

    reduce build portfolio size

--- run/addAmounts.sh
+++ run/addAmounts.sh
@@ -1,23 +1,15 @@
 echo "Add 4 amounts to cosmos on each chain for Account-0"
 ./pokt addAmount Account-0 Chain-0 cosmos 0  
 ./pokt addAmount Account-0 Chain-1 cosmos 1  
-./pokt addAmount Account-0 Chain-2 cosmos 2  
-./pokt addAmount Account-0 Chain-3 cosmos 3  
 
 echo "Add 4 amounts to cosmos on each chain for Account-1"
 ./pokt addAmount Account-1 Chain-0 cosmos 0  
 ./pokt addAmount Account-1 Chain-1 cosmos 1  
-./pokt addAmount Account-1 Chain-2 cosmos 2  
-./pokt addAmount Account-1 Chain-3 cosmos 3  
 
 echo "Add 4 amounts to cosmos on each chain for Account-2"
 ./pokt addAmount Account-2 Chain-0 cosmos 0  
 ./pokt addAmount Account-2 Chain-1 cosmos 1  
-./pokt addAmount Account-2 Chain-2 cosmos 2  
-./pokt addAmount Account-2 Chain-3 cosmos 3  
 
 echo "Add 4 amounts to cosmos on each chain for Account-3"
 ./pokt addAmount Account-3 Chain-0 cosmos 0  
 ./pokt addAmount Account-3 Chain-1 cosmos 1  
-./pokt addAmount Account-3 Chain-2 cosmos 2  
-./pokt addAmount Account-3 Chain-3 cosmos 3  
