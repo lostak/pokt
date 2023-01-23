d51aafb3b6ace85f1cc292ea90c04372ee26c743
lostak <lostak@engineer.com>
2023-01-23 00:52:12 -0800

    add addr to add chains bash

--- run/addChains.sh
+++ run/addChains.sh
@@ -1,11 +1,11 @@
 echo "Add 10 chains"
-./pokt addChain Account-0 Chain-0
-./pokt addChain Account-1 Chain-1
-./pokt addChain Account-2 Chain-2
-./pokt addChain Account-3 Chain-3
-./pokt addChain Account-4 Chain-4
-./pokt addChain Account-5 Chain-5
-./pokt addChain Account-6 Chain-6
-./pokt addChain Account-7 Chain-7
-./pokt addChain Account-8 Chain-8
-./pokt addChain Account-9 Chain-9
+./pokt addChain Account-0 Chain-0 addr
+./pokt addChain Account-1 Chain-1 addr
+./pokt addChain Account-2 Chain-2 addr
+./pokt addChain Account-3 Chain-3 addr
+./pokt addChain Account-4 Chain-4 addr
+./pokt addChain Account-5 Chain-5 addr
+./pokt addChain Account-6 Chain-6 addr
+./pokt addChain Account-7 Chain-7 addr
+./pokt addChain Account-8 Chain-8 addr
+./pokt addChain Account-9 Chain-9 addr
