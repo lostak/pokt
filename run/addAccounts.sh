966411c5a92c99e23d9f8c3869ee046f15c60e9b
lostak <lostak@engineer.com>
2023-01-23 01:54:41 -0800

    test

--- run/addAccounts.sh
+++ run/addAccounts.sh
@@ -1,5 +1,5 @@
 echo "Add 4 accounts"
-./pokt addAccount Account-0
-./pokt addAccount Account-1
-./pokt addAccount Account-2
-./pokt addAccount Account-3
+../pokt addAccount Account-0
+../pokt addAccount Account-1
+../pokt addAccount Account-2
+../pokt addAccount Account-3
