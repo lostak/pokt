f3ec2d26f1b4f973f9aba89b878c4b7773f0200b
lostak <lostak@engineer.com>
2023-01-23 02:02:46 -0800

    try change pokt dir

--- run/addTokens.sh
+++ run/addTokens.sh
@@ -1,23 +1,23 @@
 echo "Add cosmos token to each chain on Account-0"
-../pokt addToken Account-0 Chain-0 cosmos 
-../pokt addToken Account-0 Chain-1 cosmos 
-../pokt addToken Account-0 Chain-2 cosmos 
-../pokt addToken Account-0 Chain-3 cosmos 
+./pokt addToken Account-0 Chain-0 cosmos 
+./pokt addToken Account-0 Chain-1 cosmos 
+./pokt addToken Account-0 Chain-2 cosmos 
+./pokt addToken Account-0 Chain-3 cosmos 
 
 echo "Add cosmos token to each chain on Account-0"
-../pokt addToken Account-1 Chain-0 cosmos 
-../pokt addToken Account-1 Chain-1 cosmos 
-../pokt addToken Account-1 Chain-2 cosmos 
-../pokt addToken Account-1 Chain-3 cosmos 
+./pokt addToken Account-1 Chain-0 cosmos 
+./pokt addToken Account-1 Chain-1 cosmos 
+./pokt addToken Account-1 Chain-2 cosmos 
+./pokt addToken Account-1 Chain-3 cosmos 
 
 echo "Add cosmos token to each chain on Account-0"
-../pokt addToken Account-2 Chain-0 cosmos 
-../pokt addToken Account-2 Chain-1 cosmos 
-../pokt addToken Account-2 Chain-2 cosmos 
-../pokt addToken Account-2 Chain-3 cosmos 
+./pokt addToken Account-2 Chain-0 cosmos 
+./pokt addToken Account-2 Chain-1 cosmos 
+./pokt addToken Account-2 Chain-2 cosmos 
+./pokt addToken Account-2 Chain-3 cosmos 
 
 echo "Add cosmos token to each chain on Account-0"
-../pokt addToken Account-3 Chain-0 cosmos 
-../pokt addToken Account-3 Chain-1 cosmos 
-../pokt addToken Account-3 Chain-2 cosmos 
-../pokt addToken Account-3 Chain-3 cosmos 
+./pokt addToken Account-3 Chain-0 cosmos 
+./pokt addToken Account-3 Chain-1 cosmos 
+./pokt addToken Account-3 Chain-2 cosmos 
+./pokt addToken Account-3 Chain-3 cosmos 
