209dc7bdca6b0b17dceb715cefc9a51f01f401fd
lostak <lostak@engineer.com>
2023-01-23 03:15:18 -0800

    add sleep between demo amount entries

--- .github/workflows/addAmounts.sh
+++ .github/workflows/addAmounts.sh
@@ -3,6 +3,8 @@ echo -e "\nAdd 2 amounts to cosmos on each chain for Account-0 \n"
 ./pokt addAmount Account-0 Chain-0 cosmos 0  
 ./pokt addAmount Account-0 Chain-1 cosmos 1  
 
+sleep 2
+
 echo -e "\nAdd 2 amounts to cosmos on each chain for Account-1\n"
 ./pokt addAmount Account-1 Chain-0 cosmos 0  
 ./pokt addAmount Account-1 Chain-1 cosmos 1  
