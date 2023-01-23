5855e239b19fcb160298d430c9aa41f7bf71b543
lostak <lostak@engineer.com>
2023-01-23 03:19:41 -0800

    print touchup

--- .github/workflows/addAmounts.sh
+++ .github/workflows/addAmounts.sh
@@ -1,11 +1,14 @@
 # MUST CALL FROM BASE DIR FOR PATHS
 echo -e "\nAdd 2 amounts to cosmos on each chain for Account-0 \n"
 ./pokt addAmount Account-0 Chain-0 cosmos 0  
+echo -e "\nsleep 1 second\n"
 sleep 1
 ./pokt addAmount Account-0 Chain-1 cosmos 1  
+echo -e "\nsleep 1 second\n"
 sleep 1
 
 echo -e "\nAdd 2 amounts to cosmos on each chain for Account-1\n"
 ./pokt addAmount Account-1 Chain-0 cosmos 0  
+echo -e "\nsleep 1 second\n"
 sleep 1
 ./pokt addAmount Account-1 Chain-1 cosmos 1  
