904c01bc970f446865fdc4b426818c3f9227b909
lostak <lostak@engineer.com>
2023-01-23 02:04:51 -0800

    fix errouneous bash cmds

--- run/clearHistories.sh
+++ run/clearHistories.sh
@@ -1,21 +1,21 @@
 
 echo "Delete the token history for ATOM"
-bash ./pokt deleteTokenHistory AccountName Cosmos-Hub ATOM 
+ ./pokt deleteTokenHistory AccountName Cosmos-Hub ATOM 
 
 echo "Add a new token to Cosmos-Hub and then clear all the chain tokens' histories"
-bash ./pokt addToken AccountName Cosmos-Hub USDC && ./pokt deleteChainHistory AccountName Cosmos-Hub 
+ ./pokt addToken AccountName Cosmos-Hub USDC && ./pokt deleteChainHistory AccountName Cosmos-Hub 
 
 echo "Clear the Cosmos-Hub history"
-bash ./pokt deleteChainHistory AccountName Cosmos-Hub
+ ./pokt deleteChainHistory AccountName Cosmos-Hub
 
 echo "Clear AccountName History"
-bash ./pokt deleteAccountHistory AccountName 
+ ./pokt deleteAccountHistory AccountName 
 
 echo "Clear Portfolio History"
-bash ./pokt deletePortfolioHistory 
+ ./pokt deletePortfolioHistory 
 
 echo "Delete the token history for ATOM"
-bash ./pokt deleteTokenHistory AccountName Cosmos-Hub ATOM 
+ ./pokt deleteTokenHistory AccountName Cosmos-Hub ATOM 
 
 echo "Add a new amount and then clear the chain token history"
-bash ./pokt addAmount AccountName Cosmos-Hub ATOM 10 && ./pokt deleteChainHistory AccountName Cosmos-Hub 
+ ./pokt addAmount AccountName Cosmos-Hub ATOM 10 && ./pokt deleteChainHistory AccountName Cosmos-Hub 
--- run/removal.sh
+++ run/removal.sh
@@ -1,8 +1,8 @@
 echo "Remove Token named ATOM from Cosmos-Hub from Account named AccountName"
-bash ./pokt removeToken AccountName Cosmos-Hub ATOM
+./pokt removeToken AccountName Cosmos-Hub ATOM
 
 echo "Remove Chain named Cosmos-Hub from Account named AccountName"
-bash ./pokt removeChain AccountName Cosmos-Hub
+./pokt removeChain AccountName Cosmos-Hub
 
 echo "Remove Account named AccountName"
-bash ./pokt removeAccount AccountName
+./pokt removeAccount AccountName
